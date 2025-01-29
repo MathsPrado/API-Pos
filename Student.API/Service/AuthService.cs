0dea1dbdcbc18f78e9c3b86a883d6dea214e8aef
MathsPrado <matheusprado55@gmail.com>
2025-01-28 22:11:10 -0300

    Added changes to JWT

---
+++ Student.API/Service/AuthService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Org.BouncyCastle.Crypto.Generators;
+using Student.API.Service.Interface;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Student.API.Repository;
+using Student.API.Repository.Interface;
+using Student.API.Model;
+using BCrypt.Net;
+
+namespace Student.API.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IPerfilUserRepository _userRepository;
+        private readonly IConfiguration _configuration;
+
+        public AuthService(IPerfilUserRepository userRepository, IConfiguration configuration)
+        {
+            _userRepository = userRepository;
+            _configuration = configuration;
+        }
+
+        public async Task<string> AuthenticateAsync(PerfilUserDTO request)
+        {
+            var user = await _userRepository.GetUserByEmailAsync(request.Email);
+            if (user == null || !VerifyPassword(request.Password, user.PasswordHash))
+            {
+                return null;
+            }
+
+            return GenerateJwtToken(user.Email);
+        }
+
+        private bool VerifyPassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+
+        private string GenerateJwtToken(string email)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Email, email)
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["ExpirationInMinutes"])),
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256));
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public Task CreateUserAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
